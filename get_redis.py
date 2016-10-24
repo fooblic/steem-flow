4497bff14132d945fcfba6a55439b1c009d77708
fooblic <fooblic@users.noreply.github.com>
2016-10-25 00:09:49 +0200

    last block num. from redis

--- get_redis.py
+++ get_redis.py
@@ -2,6 +2,7 @@ import json
 
 def get_redis(db):
     '''Get last parsed block number from redis'''
-    head = json.loads( db.get("block_head").decode() )
-    end = int(head["end_block"])
+    redis_key = db.lindex("steem:chain", -1) # last item
+    read_stats = json.loads( db.get(redis_key).decode() )
+    end = int(read_stats["br"])
     return end
